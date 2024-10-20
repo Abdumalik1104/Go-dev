9855c6fcabb91b29fe3dd510c81a2af6639d0bfc
Abdumalik1104 <123067373+Abdumalik1104@users.noreply.github.com>
2024-10-20 23:44:32 +0500

    midterm

---
+++ midterm/project/backend/models/task.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type Task struct {
+	gorm.Model
+	ID          uint   `json:"id" gorm:"primary_key"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
